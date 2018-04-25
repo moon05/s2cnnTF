99a894470e1b1bfe832735b9b6ebd6618b56f829
Abdullah Al Mamun <amamun@u.rochester.edu>
2018-04-24 21:05:09 -0400

    added stuff

---
+++ utils/complex_utils.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import numpy as np
+
+
+#function would take in real-valued array
+#return as complex array with real part as the inital array
+#imaginary part as zeros
+
+def as_complex(x):
+	imaginary = tf.zeros([1, tf.size(x)], tf.float32)[0]
+	complex_array = tf.complex(x, imaginary)
+	return complex_array
+
+def fftshift(x, axis):
+	return 0
+
+
+sess = tf.Session()
+
+
+a = tf.constant([1.0, 2.0, 3.0])
+
+(sess.run(as_complex(a)))
